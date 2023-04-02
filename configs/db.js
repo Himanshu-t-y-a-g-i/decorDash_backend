4215f68993d1c9afeb87ec00fa28567047d599b9
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-04-02 22:06:55 +0530

    update

--- configs/db.js
+++ configs/db.js
@@ -2,6 +2,6 @@ require("dotenv").config()
 
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect(process.env.url,{ useFindAndModify: false });
+const connection = mongoose.connect(process.env.url);
 
 module.exports = { connection };
