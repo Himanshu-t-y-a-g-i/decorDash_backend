dc5f8e3b426ac8bf313e2a3bd24de18209b93286
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-04-02 17:31:19 +0530

    update

--- routes/prod.routes.js
+++ routes/prod.routes.js
@@ -55,7 +55,7 @@ prodRoutes.get("/:id", async (req, res) => {
     const { id } = req.params;
     try {
         const data = await prodModel.find({ _id: id });
-        res.status(200).send({ msg: [data], status: "success" });
+        res.status(200).send({ msg: data, status: "success" });
     } catch (e) {
         res.status(400).send({ msg: e.message })
     }
